76fabd45f62751a2f1de60019401a1788acae714
GabrieleGranzotto <gabba.granz@gmail.com>
2022-02-22 11:47:37 +0100

    prova commit Linux

---
+++ schema programma(teoria).c
@@ -0,0 +1,12 @@
+#include<stdio.h>   //direttiva al preprocessore
+#define TRUE 0      //un esempio di #define per introdurre un artificio Booleano
+                    //file di headers
+
+//possibili variabili globali perchè in nessun blocco
+//poco elegante e ottimizzato (meglio programmare con variabili locali)
+//int x=5;
+
+//funzioni (preferibilmente) sempre in testa
+
+
+
