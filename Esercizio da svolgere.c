6665b087edaaeb676c01f84e9ba828e52a53341e
GabrieleGranzotto <94189088+GabrieleGranzotto@users.noreply.github.com>
2022-03-24 15:25:52 +0100

    Delete Esercizio da svolgere.c

--- Esercizio da svolgere.c
+++
@@ -1,4 +0,0 @@
-//Chiedo Input ad un Utente fino a che un valore negativo viene inserito.
-//Per ogni numero pari inserito, la memorizzo in una lista.
-//I numeri dispari sono inseriti in un'alta lista, alla fine riporto (printf)
-//quale delle due liste sia più grande, e la somma totale dei valori in ciascuna lista.
